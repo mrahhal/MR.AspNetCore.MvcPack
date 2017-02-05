be72f8265a37e4cdb5a15cd2192b6a2b1a2defb2
Mohammad Rahhal <m.r992@hotmail.com>
2017-02-05 15:56:07 +0200

    test: ReflectionHelper.IncludeBaseTypes

---
+++ test/MR.AspNetCore.MvcPack.Tests/Internal/ReflectionHelperTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using FluentAssertions;
+using Xunit;
+
+namespace MR.AspNetCore.MvcPack.Internal
+{
+	public class ReflectionHelperTest
+	{
+		private class Base
+		{
+		}
+
+		private class Sub : Base
+		{
+		}
+
+		[Fact]
+		public void IncludeBaseTypes_InOrder()
+		{
+			var types = ReflectionHelper.IncludeBaseTypes(typeof(Sub).GetTypeInfo());
+
+			types.Should().ContainInOrder(typeof(Base).GetTypeInfo(), typeof(Sub).GetTypeInfo());
+		}
+	}
+}
